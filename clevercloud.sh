ea6d4b7f18f0fa943143e68c4cba93cbec92a5df
Loic Tosser <loic@10ten.com>
2020-01-06 23:59:08 +0400

    Fix https

--- clevercloud.sh
+++ clevercloud.sh
@@ -26,7 +26,7 @@ cat <<EOF > config.production.json
 	    "secretAccessKey": "$CELLAR_ADDON_KEY_SECRET",
 	    "region": "US",
 	    "bucket": "blog",
-	    "assetHost": "//blog.$CELLAR_ADDON_HOST",
+	    "assetHost": "https://blog.$CELLAR_ADDON_HOST",
 	    "endpoint": "$CELLAR_ADDON_HOST"
 	}
     },
