94be908b52c730c7af7587e1e41703d76b69db1c
Loic Tosser <loic@10ten.com>
2020-01-06 22:01:36 +0400

    Initial commit

---
+++ clevercloud.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+mkdir ghost # create a folder for a new local instance of Ghost
+cd ghost
+./node_modules/.bin/ghost install local
+./node_modules/.bin/ghost stop
+# generate production config file
+cat <<EOF > config.production.json
+{
+    "server": {
+	"port": 8080,
+	"host": "0.0.0.0"
+    },
+    "database": {
+	"client": "mysql",
+	"connection": {
+	    "host": "$MYSQL_ADDON_HOST",
+	    "port": $MYSQL_ADDON_PORT,
+	    "user": "$MYSQL_ADDON_USER",
+	    "password": "$MYSQL_ADDON_PASSWORD",
+	    "database": "$MYSQL_ADDON_DB"
+	}
+    },
+    "mail": {
+	"transport": "Direct"
+    },
+    "process": "local",
+    "logging": {
+	"level": "debug",
+	"transports": [
+	    "stdout"
+	]
+    },
+    "paths": {
+	"contentPath": "../../../content/"
+    }
+}
+EOF
